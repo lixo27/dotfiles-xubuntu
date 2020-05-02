5c1b161d036c629d245deb2a9672de3b627ad955
Thiago Senna <trsenna@gmail.com>
2020-05-02 20:38:59 -0300

    added back ubuntu upgrade

--- resources/scripts/install/install-editors.zsh
+++ resources/scripts/install/install-editors.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
 sudo snap install code
+
 sudo snap install phpstorm
--- resources/scripts/install/install-ubuntu.zsh
+++ resources/scripts/install/install-ubuntu.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 sudo apt update
+sudo apt upgrade
 sudo apt install \
   colordiff dos2unix gettext graphviz imagemagick \
   git-core subversion ngrep wget unzip zip \
