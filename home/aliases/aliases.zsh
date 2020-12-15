b040979cd6c049c5eca6ca067fd556cf2f1f0da5
Thiago Senna <trsenna@gmail.com>
2020-12-14 23:00:23 -0300

    removed included aliases

--- home/aliases/aliases.zsh
+++ home/aliases/aliases.zsh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
 # -- includes -----------------------------------------------
-source "${HOME}/.dotfiles/home/aliases/aliases-chocolina.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-fang.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-hope.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-lumina.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-mog.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-noel.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-serah.zsh"
-source "${HOME}/.dotfiles/home/aliases/aliases-vanille.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-chocolina.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-fang.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-hope.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-lumina.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-mog.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-noel.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-serah.zsh"
+#source "${HOME}/.dotfiles/home/aliases/aliases-vanille.zsh"
 
 # -- lorem ipsum --------------------------------------------
 # -- nullam commodo -----------------------------------------
